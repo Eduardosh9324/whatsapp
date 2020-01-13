4a85d064c941a82c4a7771ace93cd1962de9fc7a
Eduardo <eduardosh9324@gmail.com>
2020-01-13 16:24:44 -0600

    subiendo 1

---
+++ CSS grid/js/eventos.js
@@ -0,0 +1,8 @@
+function show(){
+    var elem = document.getElementById("efecto");
+    elem.style.background = '#F6F1F0';
+}
+function show1(){
+    var elem = document.getElementById("efecto");
+    elem.style.background = 'none';
+}
